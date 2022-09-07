2be13df3fc5fbe5c3fc5a3b1f5ba2b4446e70e45
wagner <wagncarv@outlook.com>
2022-09-07 15:49:47 -0300

    Primeira inserção

---
+++ formatter.erl
@@ -0,0 +1,15 @@
+-module(formatter).
+-export([month/1]).
+month(1) -> "January";
+month(2) -> "February";
+month(3) -> "March";
+month(4) -> "April";
+month(5) -> "May";
+month(6) -> "June";
+month(7) -> "July";
+month(8) -> "August";
+month(9) -> "September";
+month(10) -> "October";
+month(11) -> "November";
+month(12) -> "December";
+month(_) -> "N/A".
