a8f9c09dbc52351870d0f6ec19d5eab49cfe16c1
wagner <wagncarv@outlook.com>
2022-09-07 16:16:15 -0300

    Recursão

--- utility.erl
+++ utility.erl
@@ -1,5 +1,22 @@
 -module(utility).
 -export([factorial/1]).
+-export([reverse_list/1]).
+-export([convert_list_to_binary/1]).
 
 factorial(0) -> 1;
 factorial(N) -> N * factorial(N - 1).
+
+reverse_list(List) ->
+    reverse_list(List, []).
+
+reverse_list([], ReversedAcc) -> ReversedAcc;
+reverse_list([H|T], ReversedAcc) ->
+    reverse_list(T, [H|ReversedAcc]).
+
+convert_list_to_binary(List) ->
+    convert_list_to_binary(List, <<>>).
+
+convert_list_to_binary([], Acc) -> Acc;
+
+convert_list_to_binary([H|T], Acc) ->
+    convert_list_to_binary(T, <<Acc/binary, H>>).
